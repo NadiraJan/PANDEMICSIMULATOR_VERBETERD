d994ea2c2abef5f0ad96061f562e6ef38d089bea
NadiraJan <nadira.janbaeva@hotmail.com>
2024-01-15 15:12:21 +0100

    Initial commit

---
+++ src/be/intecbrussel/TemperatureSorter.java
@@ -0,0 +1,12 @@
+package be.intecbrussel;
+
+import java.util.Comparator;
+
+        public class TemperatureSorter implements Comparator<Patient> {
+
+                @Override
+                public int compare(Patient o1, Patient o2) {
+                        return o1.getTemperature() - o2.getTemperature();
+                }
+        }
+
